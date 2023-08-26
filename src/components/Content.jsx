9067af60df1bdabc6c90617a1d9fc92e73e886a7
hikmareta <andhika1499@gmail.com>
2023-08-26 21:06:02 +0700

    feat: adding add task feature

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 import { Heading, Checkbox, IconButton, Input, Button } from "@chakra-ui/react";
 import style from "../style/content.module.css";
-import { optionList } from "../data/optionlist";
 import { DeleteIcon } from "@chakra-ui/icons";
 
 const Content = () => {
+  const [countDone, setCountDone] = useState(0);
+  const [submitTask, setSubmitTask] = useState("");
+  const [optionList, setOptionList] = useState([
+    "Create Guest Experience Mobile Check-in",
+    "Document current CI/CD process",
+    "Perform code review for final Pillow-Talk release",
+    "Implement new color palette from design team",
+    "Fix image uploading process for guest check-in",
+    "Provide on-boarding documentation",
+  ]);
+
+  const [deleteTask, setDeleteTask] = useState([""])
+
   return (
     <div className={style.box}>
       <div className={style.main}>
@@ -17,20 +30,20 @@ const Content = () => {
         </Heading>
         <div className={style.optionlist}>
           <ul>
-              {optionList.map((item, index) => {
-                return (
-                  <li className={style.contoh} key={index}>
-                    <Checkbox style={{ borderColor: "green", color: "white" }}>
-                      {item}
-                    </Checkbox>
-                    <IconButton
-                      variant={"outline"}
-                      colorScheme="red"
-                      icon={<DeleteIcon />}
-                    ></IconButton>
-                  </li>
-                );
-              })}
+            {optionList.map((item, index) => {
+              return (
+                <li className={style.contoh} key={index}>
+                  <Checkbox style={{ borderColor: "green", color: "white" }}>
+                    {item}
+                  </Checkbox>
+                  <IconButton
+                    variant={"outline"}
+                    colorScheme="red"
+                    icon={<DeleteIcon />}
+                  ></IconButton>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <hr style={{ color: "white", marginTop: "20px" }} />
@@ -38,20 +51,38 @@ const Content = () => {
           color={"white"}
           as="h1"
           size="xl"
-          style={{ textAlign: "center", marginBottom: "20px", marginTop: "20px"}}
+          style={{
+            textAlign: "center",
+            marginBottom: "20px",
+            marginTop: "20px",
+          }}
         >
-          Done 0
+          Done {countDone}
         </Heading>
         <Heading
           color={"white"}
           as="h1"
           size="l"
-          style={{ textAlign: "start", marginBottom: "5px", marginTop: "20px"}}
+          style={{ textAlign: "start", marginBottom: "5px", marginTop: "20px" }}
         >
           Add Task
         </Heading>
-        <Input placeholder='Add Task' style={{marginBottom: "20px", color: "white"}} />
-        <Button colorScheme='teal'> Submit </Button>
+        <Input
+          placeholder="Add Task"
+          style={{ marginBottom: "20px", color: "white" }}
+          onChange={(e) => {
+            setSubmitTask(e.target.value);
+          }}
+          value={submitTask}
+        />
+        <Button 
+          colorScheme="teal"
+          onClick={() => {
+            const newArray = [...optionList, submitTask]
+            setOptionList(newArray)
+            setSubmitTask("")
+          }}
+        > Submit </Button>
       </div>
     </div>
   );
--- src/data/optionlist.jsx
+++
@@ -1,8 +0,0 @@
-export const optionList = [
-  "Create Guest Experience Mobile Check-in",
-  "Document current CI/CD process",
-  "Perform code review for final Pillow-Talk release",
-  "Implement new color palette from design team",
-  "Fix image uploading process for guest check-in",
-  "Provide on-boarding documentation"
-];
